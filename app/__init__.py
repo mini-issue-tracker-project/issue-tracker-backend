bf5c2f683d2455abefc601b3347559323af195f7
Kemal Tahir Bıcılıoğlu <sskemalss99@gmail.com>
2025-08-03 15:10:38 +0300

    take the jwt secret key into the .evn file

--- app/__init__.py
+++ app/__init__.py
@@ -17,8 +17,6 @@ def create_app():
     
     app = Flask(__name__)
     app.config.from_object(Config)
-    # JWT secret key placeholder
-    app.config['JWT_SECRET_KEY'] = 'your_jwt_secret_key_here'  # Change this in production!
     # Optionally configure JWT token options here
     # Enable CORS with specific origin
     CORS(app, origins=["http://localhost:3000"], supports_credentials=True)
--- app/config.py
+++ app/config.py
@@ -3,3 +3,4 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://default_user:default_pass@localhost:5432/default_db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "placeholder")
